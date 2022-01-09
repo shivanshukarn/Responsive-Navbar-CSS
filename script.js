ee41a3ac28670793e31d66bb6e01c4df53f110c9
Shivanshu Karn <karnshivanshu2005@gmail.com>
2022-01-09 16:15:05 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("Hello to console")
+
+const navMore = document.getElementById('navMore')
+const navUl = document.getElementById('navUl')
+const navSearch = document.getElementById('navSearch')
+navMore.addEventListener('click', () => {
+    // console.log("Clicked navMore")
+    navUl.classList.toggle('hidden')
+    navSearch.classList.toggle('hidden')
+})
